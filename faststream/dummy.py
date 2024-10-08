c2a72c2967bba1bd22b2425c3322d5db5c441f55
kumaranvpl <kumaranvpl@users.noreply.github.com>
2024-10-08 09:45:40 +0000

    fix: add missing pre-commit changes

--- faststream/dummy.py
+++ faststream/dummy.py
@@ -1,10 +1,9 @@
 
-class Dummy():
+class Dummy:
     """Dummy class to test the import of a module."""
     def __init__(self):
         pass
 
     def dummy_method(self):
         """Dummy method to test the import of a module."""
-        return 'Dummy method called'
-    
+        return "Dummy method called"
